99bdb3549b8c92910642ba949faac065a6e7ddb7
Jacques De SAGAN <jacquesde@sagan.me>
2014-12-16 16:10:21 +0800

    ...

--- 18.meta.js
+++ 18.meta.js
@@ -1,7 +1,7 @@
 
 // ==UserScript==
 // @name                18
-// @version 0.0.1
+// @version 0.0.2
 // @namespace	        https://github.com/sagan/userscripts
 // @description	        自动确认常见糟糕网站的18禁警告。支持 Getchu 和 DLSite 等网站。
 // @updateURL https://raw.githubusercontent.com/sagan/userscripts/master/18.meta.js
